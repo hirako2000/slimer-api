7405820edd7bf8122b993fd2757b95236c96ff22
Hirako <hirako2000@gmail.com>
2020-08-15 19:30:09 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,45 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const USER = $USER;
+const PASS = $PASS;
+const REPO = 'github.com/$USER/$PASS';
+ 
+const fs = require('fs')
+const git = require('simple-git');
+const remote = `https://${USER}:${PASS}@${REPO}`;
 
 export default (req, res) => {
+  var user = req.body.user
+  var srcimage = req.body.srcimage
+  var dstimage = req.body.dstimage
+  var srcuser = req.body.srcuser
+  
+  git().silent(true)
+  .clone(remote)
+  .then(() => {
+    console.log("creating branch")
+    git().branch(dstimage)
+    .then(() => {
+      
+      
+    fs.readFile('docker-push.sh', 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      var result = data.replace(/$DOCKER_USERNAME/g, req.body.dockerusername);
+      result = data.replace(/$DOCKER_PASSWORD/g, req.body.dockerpassword);
+      result = data.replace(/$SRC_USER/g, srcuser);
+      result = data.replace(/$SRC_IMAGE/g, srcimage);
+      result = data.replace(/$DST_IMAGE/g, dstimage);
+      
+      fs.writeFile('docker-push.sh', result, 'utf8', function (err) {
+         if (err) return console.log(err);
+      });
+    });
+    git().push()
+    })
+  })
+  .catch((err) => console.error('failed: ', err));
+  
   res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.json({ result: 'Triggered slimer process from ' + srcuser + '/' + srcimage + ' to ' + user + '/' + dstimage  })
 }
