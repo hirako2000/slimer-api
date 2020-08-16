44a431e2e2852f2fdc01be68a6e603de17b9f1f6
Hirako <hirako2000@gmail.com>
2020-08-16 19:54:15 +0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,7 +4,22 @@ const PASS = process.env.PASS;
 const TRAVIS_TOKEN = process.env.TRAVIS_TOKEN;
 
 const axios = require('axios');
-require('axios-debug-log')
+// Log content type
+require('axios-debug-log')({
+  request: function (debug, config) {
+    debug('Request with ' + config.headers['content-type'])
+  },
+  response: function (debug, response) {
+    debug(
+      'Response with ' + response.headers['content-type'],
+      'from ' + response.config.url
+    )
+  },
+  error: function (debug, error) {
+    // Read https://www.npmjs.com/package/axios#handling-errors for more info
+    debug('Boom', error)
+  }
+})
 
 export default (req, res) => {
   var user = req.query.user
